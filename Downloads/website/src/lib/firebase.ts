a16893539ba3499d9c9bfb345d62035ddd43e501
Ashu <bk3238512@gmail.com>
2026-05-09 18:43:57 +0530

    Fixed Firebase auth setup

--- Downloads/website/src/lib/firebase.ts
+++ Downloads/website/src/lib/firebase.ts
@@ -1,4 +1,12 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber
+} from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA9CX6QsvzGEh-nA0Idiw_LwqCH2HNYwa0",
   authDomain: "lead-academy-d9705.firebaseapp.com",
@@ -8,3 +16,14 @@ const firebaseConfig = {
   appId: "1:1090312998956:web:70bd9bd689ffe231e9c189",
   measurementId: "G-57LGDBE9T4"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () =>
+  signInWithPopup(auth, provider);
+
+export { RecaptchaVerifier, signInWithPhoneNumber };
